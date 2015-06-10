dac23c720d1e970320e7a14922f005a79cc414a8
Giovanni Opromolla <giopromolla@gmail.com>
2015-06-10 15:53:06 -0300

    agrego los barra n

--- helloworld/src/helloworld/core.clj
+++ helloworld/src/helloworld/core.clj
@@ -7,6 +7,6 @@
   (let [cr (ConsoleReader.) 
         keyint (.readCharacter cr)]
     (println (format "Got %d ('%c')!" keyint (char keyint)))
-    (if (= keyint 97) (print "Son iguales") (print "son distintos")) 
+    (if (= keyint 97) (print "Son iguales\n") (print "son distintos\n")) 
     (flush)))
 
